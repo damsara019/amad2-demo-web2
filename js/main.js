8f8fc7641888268fc89e40b04fd6565ddfdbf151
Damsara <dilshandamsara@gmail.com>
2024-11-03 01:55:30 +0530

    flow controller

--- js/main.js
+++ js/main.js
@@ -54,3 +54,107 @@ function calculateSum() {
       
   }
 }
+
+// if/else/switch
+
+// true/false,satatemwnt that true or false
+
+var num = 45;
+
+
+
+if(num>50){
+
+  console.log("helooooooo");
+
+}
+
+function getsum(){
+
+  var num1 = parseInt(document.getElementById("num1").value);
+  var num2 = parseInt(document.getElementById("num2").value);
+
+  alert(num1+num2);
+
+}
+
+// a family has 3 children. get each child weight form users input. find anerage. if averege weight >55 alert no wories.
+
+function avawei(){
+  var wei1 = parseInt(document.getElementById("wei1").value);
+  var wei2 = parseInt(document.getElementById("wei2").value);
+  var wei3 = parseInt(document.getElementById("wei3").value);
+
+  var averege = (wei1+wei2+wei3)/3
+
+  if(averege>55){
+    alert((wei1+wei2+wei3)/3);
+  }else{
+    alert("kaapan hodata");
+  }
+}
+
+var val1 = 12;
+var val2 = 5;
+
+console.log(val1%val2);
+
+
+// var num = 11;
+// var value = num%2;
+
+// if(value==0){
+  // alert("input number is even");
+// }else{
+  // alert("input number is odd");
+// }
+
+var age = 20;
+
+if(age==18){
+  console.log("Enter");
+}else if(age==16){
+  console.log("Call parents");
+}else if(age==14){
+  console.log("call police");
+}else{
+  console.log("hallo machn");
+}
+
+
+// &&, || ------> logical operator
+if(age>18 && age==10){
+
+}
+
+
+
+var dbusername = "dilshandamsara@gmail.com";
+var dbpassword = "abcd1234";
+
+function checkLogin() {
+  var enteredUsername = document.getElementById("email").value;
+  var enteredPassword = document.getElementById("password").value;
+
+  if (enteredUsername === dbusername && enteredPassword === dbpassword) {
+    alert("Welcome");
+  } else {
+    alert("Username or password incorrect");
+  }
+}
+
+var seen = 5;
+
+switch(seen){
+  case 5:
+    console.log("5 selected");
+    break;
+  case 4:
+    console.log("4 selected");
+    break;
+  case 7:
+    console.log("7 selected");
+    break;
+  default:
+    console.log("not selected");
+  }
